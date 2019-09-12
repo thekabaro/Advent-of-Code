eabfee4bd096e8df5b258e676302b76df4a8205a
Michael Jarriel <thekabaro@gmail.com>
2019-09-12 16:08:03 -0500

    Completed Challenge 1 Part 1

---
+++ 2017/1/main.py
@@ -0,0 +1,14 @@
+myFile = open("input.txt", "r")
+myString = myFile.read()
+myString = myString.strip()
+
+total = 0
+for i in range(len(myString)):
+    if (i == len(myString) - 1):
+        if (myString[i] == myString[0]):
+            total += int(myString[i])
+    else:
+        if (myString[i] == myString[i + 1]):
+            total += int(myString[i])
+
+print(total)
