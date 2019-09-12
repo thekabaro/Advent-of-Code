1cca94997f86e483ed26cf946324c3d1ad9026c2
Michael Jarriel <thekabaro@gmail.com>
2019-09-12 17:22:31 -0500

    Finished day 2 part 2

--- 2017/Day 2/Part 2/main.py
+++ 2017/Day 2/Part 2/main.py
@@ -1,19 +1,23 @@
-#myFile = open("../input.txt", "r")
+myFile = open("../input.txt", "r")
 
-#total = 0
-#while (True):
-#    myString = myFile.readline()
-#    myString = myString.strip()
-#    if (not myString):
-#        break
-#    myNums = myString.split("\t")
-#    largest = myNums[0]
-#    smallest = myNums[0]
-#    for num in myNums:
-#        if (int(num) > int(largest)):
-#            largest = num
-#        if (int(num) < int(smallest)):
-#            smallest = num
-#    total += (int(largest) - int(smallest))
+total = 0
+while (True):
+    myString = myFile.readline()
+    myString = myString.strip()
+    if (not myString):
+        break
+    myNums = myString.split("\t")
+    num1 = myNums[0]
+    num2 = myNums[0]
+    for i in range(len(myNums)):
+        for j in range(i + 1, len(myNums)):
+            if (int(myNums[i]) > int(myNums[j])):
+                if (int(myNums[i]) % int(myNums[j]) == 0):
+                    total += int(myNums[i]) / int(myNums[j])
+            elif (int(myNums[i]) < int(myNums[j])):
+                if (int(myNums[j]) % int(myNums[i]) == 0):
+                    total += int(myNums[j]) / int(myNums[i])
+            else:
+                break
 
-#print(total)
+print(int(total))
