75dd258545f80ccbfc08f9f10305ae7274179a2d
Michael Jarriel <thekabaro@gmail.com>
2019-09-12 17:10:02 -0500

    Fixed a few things

---
+++ 2017/Day 2/Part 2/main.py
@@ -0,0 +1,19 @@
+#myFile = open("../input.txt", "r")
+
+#total = 0
+#while (True):
+#    myString = myFile.readline()
+#    myString = myString.strip()
+#    if (not myString):
+#        break
+#    myNums = myString.split("\t")
+#    largest = myNums[0]
+#    smallest = myNums[0]
+#    for num in myNums:
+#        if (int(num) > int(largest)):
+#            largest = num
+#        if (int(num) < int(smallest)):
+#            smallest = num
+#    total += (int(largest) - int(smallest))
+
+#print(total)
